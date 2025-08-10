8f0cafd48687a95e5af1c7d1ebcdb94f5a98dfa7
NikoWenc <96979773+NikoWenc@users.noreply.github.com>
2025-08-10 19:38:31 +0800

    add keyboard support

--- script.js
+++ script.js
@@ -2,12 +2,23 @@ let display = document.querySelector('#display');
 let numbersButton = document.querySelectorAll('.numbers button');
 let operatorButton = document.querySelectorAll('.operators button');
 
+document.addEventListener('keydown', (event) => {
+    // only numbers will be inputed to display
+    (Number(event.key))
+        ? display.textContent = event.key
+        : event.defaultPrevented;
+
+    if (event.key === 'Backspace') display.textContent = '';
+
+});
+
 // for number buttons
 numbersButton.forEach(element => {
     // add event to number buttons
     if (element.value == 'del'){
         element.addEventListener('click', (event) => {
             display.textContent = '';
+            event.target.blur();
             event.stopPropagation();
         })
     }else {    // for del button
@@ -24,8 +35,8 @@ operatorButton.forEach(element => {
             display.textContent += ' ' + event.target.value + ' ';
             event.stopPropagation();
         })
-    }else {
-        element.addEventListener('click', (event) => {
+    }else { //compute arguments when '=' is pressed
+        element.addEventListener('click', () => {
             let argument = display.textContent;
             argument = argument.split(' ');
             let operandA = Number(argument[0]);
@@ -42,17 +53,13 @@ function calculator (operandA, operator, operandB) {
     let total;
     
     switch(operator){
-        case '+': 
-            total = operandA + operandB
+        case '+': total = operandA + operandB
             break;
-        case '-':
-            total = operandA - operandB
+        case '-': total = operandA - operandB
             break;
-        case '/':
-            total = operandA / operandB
+        case '/': total = operandA / operandB
             break;
-        case '*':
-            total = operandA * operandB
+        case '*': total = operandA * operandB
             break;
     }
     return display.textContent = total;
