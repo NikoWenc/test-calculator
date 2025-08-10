9e8e7a6a3038a078ed4d2c7e02be0bcc5b95e80b
NikoWenc <96979773+NikoWenc@users.noreply.github.com>
2025-08-10 16:13:23 +0800

    add eventListeners to number buttons

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+let display = document.querySelector('#display');
+let numbersButton = document.querySelectorAll('.numbers button');
+let operatorButton = document.querySelectorAll('.operators button');
+
+// for number buttons
+numbersButton.forEach(element => {
+    // add event to number buttons
+    element.addEventListener('click', (event) => {
+        if(event.target.value !== 'del') {
+            display.textContent += event.target.value;
+        }
+    });
+});
+
+
+
+
 let operandA;
 let operator;
 let operandB;
